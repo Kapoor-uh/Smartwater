9651eb69d978a10038a8cafeded1a7b2ad220349
Petteri Timonen <petteri.x.timonen@helsinki.fi>
2016-04-23 21:24:29 +0300

    Add weighted rainfall calculation

--- main.py
+++ main.py
@@ -18,16 +18,24 @@ rainfall_window = 1
 pumps = parsePumps.ParseHighQualityPumps()
 flow_filter_percentile = 90
 
-while rainfall_window <= 4 * 7 * 24:
-	for pump in pumps[0:40]:
-		cursor = pumpData.getRainfallPumpingOutput(cursor, pump, 0, 24, -rainfall_window, 0,4,10)
+def GetWeightedRainfallAndFlow(cursor, pump, window, n_windows):
+	flow = None
+	weighted_rainfall = None
+	for i in range(0, n_windows):
+		cursor = pumpData.getRainfallPumpingOutput(cursor, pump, 0, 24, -window * (i + 1), -window * i, 4,10)
 		response = cursor.fetchall()
 		response = map(list, zip(*response))
 		flow = np.array(response[1])
 		rainfall = np.array(response[2])
+		weighted_rainfall = rainfall if i == 0 else weighted_rainfall + rainfall / (2 ** i)
+	return flow, weighted_rainfall
+
+while rainfall_window <= 4 * 7 * 24:
+	for pump in pumps[0:20]:
+		flow, rainfall = GetWeightedRainfallAndFlow(cursor, pump, rainfall_window, 4)
 		threshold = np.percentile(flow, flow_filter_percentile)
 		flow_rainfall = zip(*np.array(zip(flow, rainfall))[np.less_equal(flow, threshold)])
 		correlation = np.corrcoef(*flow_rainfall)[0, 1]
-		print(response[0][0], "window h: ", rainfall_window, "correlation ", round(correlation, 3))
+		print(pump, "window h: ", rainfall_window, "correlation ", round(correlation, 3))
 		plotter.save_figure_pump_rain(*flow_rainfall, name=str(rainfall_window) + "h-" + pump, folder='plots')
 	rainfall_window *= 2
