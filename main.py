b604906ad109e6b16aec124459baed509fd22c35
Petteri Timonen <petteri.x.timonen@helsinki.fi>
2016-04-23 20:06:33 +0300

    Add filtering

--- main.py
+++ main.py
@@ -16,14 +16,18 @@ cursor = cnxn.cursor()
 
 rainfall_window = 1
 pumps = parsePumps.ParseHighQualityPumps()
+flow_filter_percentile = 90
+
 while rainfall_window <= 4 * 7 * 24:
 	for pump in pumps[0:40]:
 		cursor = pumpData.getRainfallPumpingOutput(cursor, pump, 0, 24, -rainfall_window, 0,4,10)
 		response = cursor.fetchall()
 		response = map(list, zip(*response))
-		flow = response[1]
-		rainfall = response[2]
-		correlation = np.corrcoef(rainfall, flow)[0, 1]
+		flow = np.array(response[1])
+		rainfall = np.array(response[2])
+		threshold = np.percentile(flow, flow_filter_percentile)
+		flow_rainfall = zip(*np.array(zip(flow, rainfall))[np.less_equal(flow, threshold)])
+		correlation = np.corrcoef(*flow_rainfall)[0, 1]
 		print(response[0][0], "window h: ", rainfall_window, "correlation ", round(correlation, 3))
-		plotter.save_figure_pump_rain(flow, rainfall, str(rainfall_window) + "h-" + pump, 'plots')
+		plotter.save_figure_pump_rain(*flow_rainfall, name=str(rainfall_window) + "h-" + pump, folder='plots')
 	rainfall_window *= 2
