ab4fd1b7e659d9479db27e7c0dd82f5d84a94a58
Sebastian Hojas <sebastian.hojas@irrelvant.at>
2016-04-24 12:06:20 +0300

    Added rainfall window increment bug

--- main.py
+++ main.py
@@ -27,4 +27,3 @@ for rainfall_window in [4,8,16,24,48,96,128]:
 		correlation = np.corrcoef(rainfall, flow)[0, 1]
 		print(response[0][0], "window h: ", rainfall_window, "correlation ", round(correlation, 3))
 		plotter.save_figure_pump_rain(*flow_rainfall, name=str(rainfall_window) + "h-" + str(rainfall_window_start) + "h-" + pump, folder='plots')
-	rainfall_window *= 2
