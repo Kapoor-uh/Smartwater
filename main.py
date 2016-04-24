2cd415d4dfae2c3ec60adaeb58fb8c54e6a9d3cf
Sebastian Hojas <sebastian.hojas@irrelvant.at>
2016-04-24 11:55:48 +0300

    added slide window

--- main.py
+++ main.py
@@ -16,14 +16,15 @@ cursor = cnxn.cursor()
 
 rainfall_window = 1
 pumps = parsePumps.ParseHighQualityPumps()
-while rainfall_window <= 4 * 7 * 24:
+for rainfall_window in [4,8,16,24,48,96,128]:
 	for pump in pumps[0:40]:
-		cursor = pumpData.getRainfallPumpingOutput(cursor, pump, 0, 24, -rainfall_window, 0,4,10)
+		rainfall_window_start = rainfall_window/4
+		cursor = pumpData.getRainfallPumpingOutput(cursor, pump, 0, 24, -rainfall_window, -rainfall_window_start,4,10)
 		response = cursor.fetchall()
 		response = map(list, zip(*response))
 		flow = response[1]
 		rainfall = response[2]
 		correlation = np.corrcoef(rainfall, flow)[0, 1]
 		print(response[0][0], "window h: ", rainfall_window, "correlation ", round(correlation, 3))
-		plotter.save_figure_pump_rain(flow, rainfall, str(rainfall_window) + "h-" + pump, 'plots')
+		plotter.save_figure_pump_rain(*flow_rainfall, name=str(rainfall_window) + "h-" + str(rainfall_window_start) + "h-" + pump, folder='plots')
 	rainfall_window *= 2
